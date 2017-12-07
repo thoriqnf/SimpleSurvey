fea440a027b2abd5bb3bc1be3c1466824e30d642
thoriqnf <thoriqnfaizal@gmail.com>
2017-12-07 21:24:13 +0700

    adding form and state function

--- web/src/App.js
+++ web/src/App.js
@@ -2,15 +2,43 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      name: "",
+      answers: {
+        q1: "",
+        q2: "",
+        q3: "",
+        q4: ""
+      },
+      submitted: false
+    };
+  }
   render() {
+    var user;
+    var questions;
+    if (this.state.name && this.state.submitted === false) {
+    } else if (!this.state.name && this.state.submitted === false) {
+      user = (
+        <span>
+          <h2>Please enter your name to begin the survey</h2>
+          <form>
+            <input type="text" placeholder="Enter Name..." ref="name" />
+          </form>
+        </span>
+      );
+      questions = "";
+    } else if (this.state.submitted === true) {
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="App text-center">
+        <div className="App-header">
           <h1 className="App-title">Simple Survey</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </div>
+        <div class="text-center">{user}</div>
       </div>
     );
   }
